f3cc07db4f13756cab6ec014701157cfa2eab282
YanJian1990 <173048649@qq.com>
2022-07-07 14:06:08 +0800

    Update xulei.js

--- Scripts/xulei.js
+++ Scripts/xulei.js
@@ -1,7 +1,7 @@
 /*
 surge 4
 [Script]
-迅雷 = type=http-response,pattern=https://xluser-ssl.xunlei.com/xluser.core.login/v3/getuserinfo,script-path=https://raw.githubusercontent.com/YanJian1990/Surge/main/xulei.js?token=GHSAT0AAAAAABTVE3SI6ZM7VRILWIJX2OUSYVSQMLQ,requires-body=1,max-size=0
+迅雷 = type=http-response,pattern=https://xluser-ssl.xunlei.com/xluser.core.login/v3/getuserinfo,script-path=https://raw.githubusercontent.com/YanJian1990/Surge/main/Scripts/xulei.js,requires-body=1,max-size=0
 [MITM]
 hostname = %APPEND% xluser-ssl.xunlei.com
 */
