1ec40d14d372a39338c516ce7073afadf0b0b271
JamesYan <173048649@qq.com>
2022-07-07 17:03:37 +0800

    Update Ulike.js

--- Scripts/Ulike.js
+++ Scripts/Ulike.js
@@ -1,7 +1,7 @@
 /*
 醒图 轻颜相机 Ulike 蒸汽波相机(vaporcam) 解锁VIP
 
-轻颜相机 = type=http-response,pattern=^https?:\/\/commerce-.*api\.faceu\.mobi\/commerce\/v1\/subscription\/user_info,requires-body=1,max-size=0,script-path=https://github.com/JamesYan6666/Surge/edit/main/Scripts/Ulike.js,script-update-interval=-1
+轻颜相机 = type=http-response,pattern=^https?:\/\/commerce-.*api\.faceu\.mobi\/commerce\/v1\/subscription\/user_info,requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/JamesYan6666/Surge/main/Scripts/Ulike.js,script-update-interval=-1
 
 https://(commerce-.*api|pay).(faceu|wecut).(com|mobi)/(commerce|apple)/(iosAppVerifyReceipt.php|v1/subscription/user_info) 
 
