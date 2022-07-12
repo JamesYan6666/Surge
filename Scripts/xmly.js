f0818c484f5cf94fd499727734738011fc83e1bf
JamesYan <173048649@qq.com>
2022-07-12 14:33:18 +0800

    Update xmly.js

--- Scripts/xmly.js
+++ Scripts/xmly.js
@@ -1,26 +1,4 @@
 
 var body = JSON.parse($response.body);
-body.data.vipExpireTime = 365;
-body.data.vipLevel = 3;
-body.data.isVip = true;
-body.data.vipLevel = 4;
-body.data.logoType = 0;
-// var obj = {
-//     "realUid": 250171422,
-//     "logoType": 0,
-//     "uid": 250171422,
-//     "isLoginBan": false,
-//     "mobile": "159****2780",
-//     "isVip": true,
-//     "logoPic": "\/\/imagev2.xmcdn.com\/group61\/M0A\/7C\/F3\/wKgMZl0wBcnxgorbAABWZX_GLtM979.jpg",
-//     "anchorGrade": -1,
-//     "totalVipDays": 81,
-//     "isVerified": false,
-//     "vipLevel": 3,
-//     "userGrade": 0,
-//     "vipExpireTime": 12,
-//     "isRobot": false,
-//     "nickname": "听友250171422",
-//     "verifyType": 0
-// };
+body.data.currentUser.isVip = true;
 $done({ body: JSON.stringify(obj) });
