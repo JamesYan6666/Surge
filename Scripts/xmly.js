c8dc65032840a74d0310829805d1b50433e347cb
JamesYan <173048649@qq.com>
2022-07-12 15:08:12 +0800

    Update xmly.js

--- Scripts/xmly.js
+++ Scripts/xmly.js
@@ -1,9 +1,10 @@
 
 var body = $response.body;
-var obj = JSON.parse(body);
-obj.data.isVip = true;
-obj.data.isSimpleVip = true;
-obj.data.vipExpireTime = 365;
-obj.data.vipLevel = 6;
-body = JSON.stringify(obj);
+var obj1 = JSON.parse(body);
+obj1.data.isVip = true;
+obj1.data.isSimpleVip = true;
+obj1.data.vipExpireTime = 365;
+obj1.data.vipLevel = 6;
+
+body = JSON.stringify(obj1);
 $done(body);
