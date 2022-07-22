9392ec0a3b2f1546285dd69e3414fedb610fa2d1
JamesYan <173048649@qq.com>
2022-07-22 10:52:30 +0800

    Update BdCloudInfo.js

--- Scripts/BdCloudInfo.js
+++ Scripts/BdCloudInfo.js
@@ -2,6 +2,7 @@
 var body = $response.body;
 var obj = JSON.parse(body);
 obj.user_info.is_svip = 1;
+obj.user_info.is_vip = 1;
 obj.is_vip = 1;
 obj.user_info.username = "永久svip";
 obj.user_info.is_plus_buy = 1;
