6614a121ea9121316ed8ecbfc038b571225d5438
JamesYan <173048649@qq.com>
2022-07-26 15:28:44 +0800

    Update BdCloudLogin.js

--- Scripts/BdCloudLogin.js
+++ Scripts/BdCloudLogin.js
@@ -1,8 +1,8 @@
 
 var body = $response.body;
 var obj = JSON.parse(body);
-obj.login_info.vip_type = "21";
-obj.login_info.vip_identity = "21";
+obj.login_info.vip_type = "49";
+obj.login_info.vip_identity = "49";
 obj.login_info.username = "永久Vip";
 obj.login_info.vip_level = 7;
 $done({body: JSON.stringify(obj)});
